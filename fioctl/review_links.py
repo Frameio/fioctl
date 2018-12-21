cb9a11da61e8b02e76e6bb9dba5c616ec3611ae3
Michael Guarino <mguarino@frame.io>
2018-12-21 00:24:25 -0500

    Add review_links get

--- fioctl/review_links.py
+++ fioctl/review_links.py
@@ -2,12 +2,22 @@ import click
 from . import fio
 from . import utils
 from .config import column_default
+from .fio import fio_client
 
 DEFAULT_COLS = column_default('review_links', "id,name,item_count,view_count,short_url,expires_at")
 
 @click.group()
 def review_links():
     """Review link related commands"""
+
+@review_links.command(help="Gets a review link")
+@click.argument("review_link_id")
+@click.option('--format', type=utils.FormatType(), default='table')
+@click.option('--columns', default=DEFAULT_COLS)
+def get(review_link_id, format, columns):
+    review_links = fio_client()._api_call('get', f"/review_links/{review_link_id}")
+
+    format(review_links, cols=columns)
 
 @review_links.command(help="Lists review links for a project")
 @click.argument("project_id")
