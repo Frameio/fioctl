b1c68881ba2ea8482c253068047ee187bca3beae
Jeff <jeff@jeffhq.com>
2020-03-31 15:47:21 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
   long_description = f.read()
 
 setup(name='fioctl',
-      version='1.1',
+      version='1.1.1',
       description='Frame.io cli',
       long_description=long_description,
       packages=['fioctl'],
